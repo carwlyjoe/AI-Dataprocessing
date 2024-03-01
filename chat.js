41359fc0e77c4005c4d60754b209edd13c222fc1
carwlyjoe <130526582+carwlyjoe@users.noreply.github.com>
2024-03-01 15:15:59 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -64,8 +64,8 @@ document.addEventListener('DOMContentLoaded', initializeSettings);
 // 定义全局变量来存储消息历史
 
 async function sendMessage(text) {
-  const currentApiKey = localStorage.getItem('apikey') || '54e22def5b8f8b613c1ac05c267a878137bc369ccc60bd50';
-  const currentProxyUrl = localStorage.getItem('proxyUrl') || 'https://sapi.onechat.fun/v1/chat/completions';
+  const currentApiKey = localStorage.getItem('apikey') || '';
+  const currentProxyUrl = localStorage.getItem('proxyUrl') || 'https://2993212868.top/v1/chat/completions';
   const selectedModel = localStorage.getItem('selectedModel') || 'gpt-3.5-turbo-16k';
   const temperature = localStorage.getItem('temperature') || 0.5;
   const topP = localStorage.getItem('topP') || 1;
